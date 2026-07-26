13fa5847f6c26dc2993c40504202051c04148683
Mhmd Rayan <m7mddotm7@gmail.com>
2026-07-26 16:19:25 +0300

    feat: Add BlockType enum and block_to_block_type function with tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,16 @@
 from textnode import TextNode, TextType
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+    
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
@@ -11,3 +23,32 @@ def markdown_to_blocks(markdown: str) -> list[str]:
             new_blocks.append(block)
 
     return new_blocks
+
+def block_to_block_type(block) -> BlockType:
+    
+    for i in range(1, 7):
+        if block.startswith("#" * i + " "):
+            return BlockType.HEADING
+        
+        
+    if block.startswith("```\n") and block.endswith("```"):
+     return BlockType.CODE
+ 
+    lines = block.split("\n")
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    
+    is_ordered = True
+    expected_number = 1
+    for line in lines:
+        if not line.startswith(f"{expected_number}. "):
+            is_ordered = False
+            break
+        expected_number += 1
+    if is_ordered:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks , BlockType, block_to_block_type
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -65,6 +65,113 @@ Paragraph
         blocks = markdown_to_blocks("")
 
         self.assertEqual(blocks, [])
+
+    # Test Block To Block '14'
+
+    def test_heading(self):
+        self.assertEqual(
+            block_to_block_type("### This is a heading"),
+            BlockType.HEADING,
+        )
+
+    def test_heading_six_hashes(self):
+        self.assertEqual(
+            block_to_block_type("###### Heading"),
+            BlockType.HEADING,
+        )
+
+    def test_invalid_heading_without_space(self):
+        self.assertEqual(
+            block_to_block_type("###Heading"),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_invalid_heading_seven_hashes(self):
+        self.assertEqual(
+            block_to_block_type("####### Heading"),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_code(self):
+        block = "```\nprint('Hello')\n```"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.CODE,
+        )
+
+    def test_invalid_code_without_newline(self):
+        block = "```print('Hello')```"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_quote(self):
+        block = "> First line\n>Second line\n> Third line"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.QUOTE,
+        )
+
+    def test_invalid_quote(self):
+        block = "> First line\nSecond line"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_unordered_list(self):
+        block = "- First\n- Second\n- Third"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.UNORDERED_LIST,
+        )
+
+    def test_invalid_unordered_list(self):
+        block = "- First\nSecond\n- Third"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_ordered_list(self):
+        block = "1. First\n2. Second\n3. Third"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.ORDERED_LIST,
+        )
+
+    def test_ordered_list_wrong_start(self):
+        block = "2. First\n3. Second"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_ordered_list_wrong_sequence(self):
+        block = "1. First\n3. Second\n4. Third"
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
+    def test_paragraph(self):
+        block = "This is a normal paragraph.\nIt has another line."
+
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH,
+        )
+
 
 
 if __name__ == "__main__":

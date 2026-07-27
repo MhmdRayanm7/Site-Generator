31d8a2107c8851b79738313b21d248a5b7a03589
Mhmd Rayan <m7mddotm7@gmail.com>
2026-07-27 18:55:02 +0300

    feat: add recursive static asset copying

--- src/main.py
+++ src/main.py
@@ -1,10 +1,42 @@
 
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_static(source, destination):
+    if os.path.exists(destination):
+            shutil.rmtree(destination)
+    os.mkdir(destination)
+    copy_directory(source, destination)
+    
+    
+def copy_directory(source, destination):
+    items = os.listdir(source)
+
+    for item in items:
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+        
+        if os.path.isfile(source_path):
+            print("File")
+            shutil.copy(source_path,destination_path)
+            
+        else:
+            print("Directory")
+            os.mkdir(destination_path)
+            copy_directory(source_path,destination_path)
+
+        print(f"Source: {source_path}")
+        print(f"Destination: {destination_path}")
+
+
 
 
 def main():
     node = TextNode("Hello, World!", TextType.TEXT, url="https://example.com")
     print(node)
+    copy_static("static", "public")
+
 
 if __name__ == "__main__":
     main()
